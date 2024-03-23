9a2a54e04a2764f6952a881b811c3695e90ac0b5
David Hurley <dhurls99@gmail.com>
2024-03-23 17:34:05 -0400

    deploy bug fiz

--- hardhat/ignition/modules/Frame.ts
+++ hardhat/ignition/modules/Frame.ts
@@ -1,8 +1,5 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
-
 const FramesModule = buildModule('FramesModule', (m) => {
   const lock = m.contract('Frames', [
     '0xA8f3C4947599846971bdac93fE7077De25647816',
